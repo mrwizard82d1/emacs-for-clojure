d8a7b7a2c9eedb82260b32876129bde5f06f95a4
Larry Jones <mrwizard82d1@gmail.com>
2015-03-04 21:34:10 -0600

    Changed default font. Hide tool bar. Displayed menu bar.
    When I customized the tool bar, emacs saved other stuff.
    I'll probably get rid of it at some point.

--- init.el
+++ init.el
@@ -130,3 +130,20 @@
 ;; Langauage-specific
 (load "setup-clojure.el")
 (load "setup-js.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(coffee-tab-width 2)
+ '(scroll-bar-mode nil)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil)
+ '(menu-bar-mode 1))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Lucida Console" :foundry "outline" :slant normal :weight normal :height 98 :width normal)))))
